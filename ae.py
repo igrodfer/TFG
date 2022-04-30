e4ed9ece92f7c03a9c01f54851c81b10cce57cba
igrodfer <62602009+igrodfer@users.noreply.github.com>
2022-04-30 16:59:48 +0200

    movido clase de autoencoder a paquete externo

---
+++ ae.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+
+class AutoEncoder(nn.Module):
+    def __init__(self, activation=nn.ReLU(),
+                 input_size=3*8*8, hidden_sizes=[32*3,8,32*3]):
+        super(AutoEncoder, self).__init__()
+        self.input_size = input_size
+
+        # Here we initialize our activation and set up our two linear layers
+        self.activation = activation
+        self.fc1 = nn.Linear(input_size, hidden_sizes[0])
+        self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
+        self.fc3 = nn.Linear(hidden_sizes[1], hidden_sizes[2])
+        self.fc4 = nn.Linear(hidden_sizes[2], input_size)
+
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = self.decode(x)
+
+        return x
+    
+    def encode(self,x):
+        x = x.view(-1, 3*8*8)
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+
+        return x
+    
+    def decode(self,x):        
+        x = self.activation(x)
+        x = self.fc3(x)        
+        x = self.activation(x)
+        x = self.fc4(x)
+
+        return x
