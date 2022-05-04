59ed9aaa3ecc3a032fef5e8180c2c7c6190122a2
igrodfer <62602009+igrodfer@users.noreply.github.com>
2022-05-04 13:08:46 +0200

    Crada función de comprimir

--- autoencoder_compressor.py
+++ autoencoder_compressor.py
@@ -7,28 +7,23 @@ import torch
 
 
 class Compressor_Decompressor:
-    def __init__(self,model_path,chunk_size=8) -> None:
+    def __init__(self,model_path:str,chunk_size=8) -> None:
         self.tile_size = chunk_size
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         self.init_autoencoder(model_path)
 
+    def init_autoencoder(self,model_path:str):
+        model = AutoEncoder.load_autoencoder(model_path)
+        self.model = model.to(self.device)
 
-
-    def init_autoencoder(self,model_path:string):
-        self.model = AutoEncoder.load_autoencoder(model_path)
-        self.send_model_to_device()
-
-    def send_model_to_device(self):
-        self.model = self.model.to(self.device)
-
-    def send_image_to_device(self,image_tensor:torch.tensor) -> torch.tensor:
+    def send_image_to_device(self,image_tensor:torch.Tensor) -> torch.Tensor:
         return image_tensor.to(self.device)
 
-    def load_image_as_np(image_path:string)->np.array:
+    def load_image_as_np(self,image_path:str)->np.ndarray:
         return np.asarray(Image.open(image_path).convert('RGB'), dtype=np.uint8)
 
-    def store_image_from_np(image_path:string,data:np.array,format='RGB')->Image:
+    def store_image_from_np(image_path:str,data:np.ndarray,format='RGB')->Image:
         img = Image.fromarray(data, format)
         img.save(image_path)
         return img
@@ -56,7 +51,7 @@ class Compressor_Decompressor:
         return np.concatenate(tiled_array,axis=0)
 
 
-    def rebuild_image(self,tile_array,image_size):
+    def rebuild_image(self,tile_array:np.ndarray,image_size):
         img_height, img_width, channels = image_size
         
         tile_rows = int(np.ceil(img_height/self.tile_size))
@@ -73,13 +68,13 @@ class Compressor_Decompressor:
 
         return tile_array[:img_height,:img_width]
     
-    def apply_compress_function(self,tile_tensor:torch.tensor) -> torch.tensor:
-        return self.model.encode(self.send_image_to_device(tile_tensor))
+    def apply_compress_function(self,tile_tensor:torch.Tensor) -> torch.Tensor:
+        return self.model.encode(tile_tensor)
 
-    def apply_decompression_function(self,encoded_tile_tensor:torch.tensor) -> torch.tensor:
+    def apply_decompression_function(self,encoded_tile_tensor:torch.Tensor) -> torch.Tensor:
         return self.model.decode(encoded_tile_tensor)
 
-    def make_tensor(self,tile_list_array:np.array)-> torch.tensor:    
+    def make_tensor(self,tile_list_array:np.ndarray)-> torch.Tensor:    
         tile_list_array = tile_list_array.swapaxes(2,3)
         tile_list_array = tile_list_array.swapaxes(1,2)
         
@@ -87,10 +82,24 @@ class Compressor_Decompressor:
         return tile_list_tensor /255
 
 
-    def retrieve_array(self,decoded_tile_tensor:torch.tensor) -> np.array:
+    def retrieve_array(self,decoded_tile_tensor:torch.Tensor) -> np.ndarray:
         res = decoded_tile_tensor.detach().cpu().reshape(-1,3,self.tile_size,self.tile_size)
         res = (res.numpy() * 255).astype('uint8')
         res = res.swapaxes(1,2)    
         res = res.swapaxes(2,3)
         return res
+    
+    def compress_image(self,image_path:str):
+        image_np                    = self.load_image_as_np(image_path)
+        image_size                  = image_np.shape
+        tile_list_np                = self.segment_image(image_np,pad_type='reflect')
+        tile_list_tensor            = self.make_tensor(tile_list_np)
+        tile_list_tensor_cuda       = self.send_image_to_device(tile_list_tensor)
+        compressed_image_tensor     = self.apply_compress_function(tile_list_tensor_cuda)
+        clean_c                     = compressed_image_tensor.detach().cpu()
+        return clean_c, image_size
+    
+ 
+        
+
 
