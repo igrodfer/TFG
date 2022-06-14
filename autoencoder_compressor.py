d835b1ea4fb85bd89b237bd3026a5c5f25326884
igrodfer <62602009+igrodfer@users.noreply.github.com>
2022-06-14 20:52:55 +0200

    añadido uso de modelos de color diferente

--- autoencoder_compressor.py
+++ autoencoder_compressor.py
@@ -1,4 +1,5 @@
 import string
+from typing import Tuple
 import numpy as np
 import os
 from PIL import Image
@@ -8,11 +9,11 @@ import ae
 
 
 class Compressor_Decompressor:
-    def __init__(self,model_path:str,model_type,chunk_size=8,compression_out=8,color_type="RGB") -> None:
+    def __init__(self,model_path:str,model_type,chunk_size=8,compression_out=8,color_format="RGB") -> None:
         self.tile_size = chunk_size
         self.compression_out=compression_out
         self.model_type = model_type
-        self.color_type = color_type
+        self.color_type = color_format
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         self.init_autoencoder(model_path)
@@ -28,7 +29,7 @@ class Compressor_Decompressor:
         return np.asarray(Image.open(image_path).convert(self.color_type), dtype=np.uint8)
         
     def store_image_from_np(self,image_path:str,data:np.ndarray)->Image:
-        img = Image.fromarray(data, self.color_type)
+        img = Image.fromarray(data, self.color_type).convert('RGB')
         img.save(image_path)
         return img
 
@@ -73,16 +74,19 @@ class Compressor_Decompressor:
         return tile_array[:img_height,:img_width]
     
     def apply_compress_function(self,tile_tensor:torch.Tensor) -> torch.Tensor:
-        return self.model.encode(tile_tensor)
+        with torch.no_grad():
+            return self.model.encode(tile_tensor)
 
     def apply_decompression_function(self,encoded_tile_tensor:torch.Tensor) -> torch.Tensor:
-        return self.model.decode(encoded_tile_tensor)
+        with torch.no_grad():
+
+            return self.model.decode(encoded_tile_tensor)
 
     def make_tensor(self,tile_list_array:np.ndarray)-> torch.Tensor:    
         tile_list_array = tile_list_array.swapaxes(2,3)
         tile_list_array = tile_list_array.swapaxes(1,2)
         
-        tile_list_tensor = torch.from_numpy(tile_list_array.astype('float32')).reshape(-1,3*self.tile_size*self.tile_size)
+        tile_list_tensor = torch.from_numpy(tile_list_array.astype('float32')).reshape(-1,self.tile_size,self.tile_size,3)
         return tile_list_tensor /255
 
 
@@ -91,8 +95,8 @@ class Compressor_Decompressor:
         res = (res.numpy() * 255)
         res = res.swapaxes(1,2)    
         res = res.swapaxes(2,3)
-        return res.astype('uint8') if self.color_type != "YCbCr" else Compressor_Decompressor.ycbcr2rgb(res) 
-    
+        return res.astype('uint8') 
+
     @staticmethod
     def ycbcr2rgb(im):
         xform = np.array([[1, 0, 1.402], [1, -0.34414, -.71414], [1, 1.772, 0]])
@@ -104,7 +108,7 @@ class Compressor_Decompressor:
         return np.uint8(rgb)
 
     @staticmethod
-    def scale_array(the_array:np.ndarray):
+    def scale_array(the_array:torch.Tensor)-> Tuple[torch.Tensor,Tuple[float,float]]:
         max = the_array.max()
         min = the_array.min()
         
@@ -112,7 +116,7 @@ class Compressor_Decompressor:
         return interpolated_array.to(torch.uint8) , (max,min)
 
     @staticmethod
-    def descale_array(interpolated_array,interval):
+    def descale_array(interpolated_array:torch.Tensor,interval:Tuple[float,float])-> torch.Tensor:
         max,min = interval
         the_array = interpolated_array * (max - min) / 255 + min
         return the_array.to(torch.float)
@@ -142,7 +146,7 @@ class Compressor_Decompressor:
         end_image                   = self.rebuild_image(decompresssed_image,image_size)
         
         if return_image:
-            return end_image
+            return end_image if self.color_type == "RGB" else Compressor_Decompressor.ycbcr2rgb(end_image)
         self.store_image_from_np(destination_path, end_image)
 
 
