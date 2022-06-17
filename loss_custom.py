cadcb65e6c7b0dd72397f4cd8e61f1a7288ae451
igrodfer <62602009+igrodfer@users.noreply.github.com>
2022-06-17 15:29:34 +0200

    añadido parámtro de escala a error por amplitud

--- loss_custom.py
+++ loss_custom.py
@@ -2,7 +2,13 @@ from bz2 import compress
 import torch
 import torch.nn as nn
 
-def amplitude_reduced_MSE(output,compressed,target):
+def get_amplitude_reduced_MSE(l=0.1):
+    def amplitude_reduced_MSE(output,compressed,target):
+        loss = torch.mean((output-target)**2)
+        amplitude = torch.mean(compressed**2)
+        return loss + l * amplitude
+    return amplitude_reduced_MSE
+
+def custom_MSE(output,compressed,target):
     loss = torch.mean((output-target)**2)
-    amplitude = compressed**2
-    return loss + amplitude
+    return loss
